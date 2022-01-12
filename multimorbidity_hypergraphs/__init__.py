122b818594c761e285b3ff00f627d84d5e752608
Jim Rafferty <j.m.rafferty@swansea.ac.uk>
2022-01-12 09:28:43 +0000

    Incremented version number for new release.

--- multimorbidity_hypergraphs/__init__.py
+++ multimorbidity_hypergraphs/__init__.py
@@ -1,5 +1,5 @@
 """
 Large scale data analysis with hypergraphs
 """
-__version__ = "0.3.4"
-from .hypergraph_tools import Hypergraph#, randomize_weights
+__version__ = "0.3.5"
+from .hypergraph_tools import Hypergraph
