abfbed255882cd3bd6345a5e7bbbccbb0bf99a59
Jim Rafferty <james.m.rafferty@gmail.com>
2023-08-27 09:59:28 +0100

    Added a stub data structure for the hypergraph, and started thinking about the requirements for the internal data.

--- multimorbidity_hypergraphs_rs/src/lib.rs
+++ multimorbidity_hypergraphs_rs/src/lib.rs
@@ -1,14 +1,40 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+
+// directives to be used during development only. 
+#[allow(dead_code)]
+// done
+
+
+#[derive(Default)]
+pub struct Hypergraph {
+	incidence_matrix: Option<Vec<u8>>, // TODO(jim): decide whether this containiner is right
+	edge_weights: Option<Vec<f32>>,
+	node_weights: Option<Vec<f32>>,
+	edge_list: Option<Vec<u8>>, // TODO(jim): decide on this (internal) type
+	node_list: Option<Vec<u8>>, // TODO(jim): decide on this (internal) type 
 }
 
+// options for the incidence matrix:
+// Vec<u8> and cause myself all sorts of pain with indexing
+// Multiarray crate: Array2D::new([3, 2], 0); 
+// Vec<Vec<u8>> probably too slow
+
+
+// Idiomatic rust is apparently to have tests and code in the same file
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn instantiated() {
+		//     Tests the instantiation of the hypergraph object.
+		//     Pretty simple test as all internal state is set to None.
+		
+        let h = Hypergraph{..Default::default()};
+		
+        assert!(h.incidence_matrix.is_none());
+		assert!(h.edge_weights.is_none());
+		assert!(h.node_weights.is_none());
+		assert!(h.edge_list.is_none());
+		assert!(h.node_list.is_none());
     }
 }
