996d8fb07373310d10311ea51671988b4e0fc356
Jim Rafferty <james.m.rafferty@gmail.com>
2023-08-28 17:39:22 +0100

    Started adding the code with reduced_powerset. Having some problems with itertools...

--- multimorbidity_hypergraphs_rs/src/lib.rs
+++ multimorbidity_hypergraphs_rs/src/lib.rs
@@ -1,22 +1,41 @@
 
 // directives to be used during development only. 
-#[allow(dead_code)]
+//#[allow(dead_code)]
+//#[allow(unused_imports)]
 // done
+
+use ndarray::prelude::*;
+use ndarray::{Array2, ArrayView1};
+use itertools::Itertools;
+
+use std::collections::HashSet;
+
+fn reduced_powerset(data_row: ArrayView1<u8>) -> HashSet<Vec<u8>> {
+
+		let mut out = HashSet::new();
+		let indices = data_row
+			.iter()
+			.enumerate()
+			.filter(|(_, &r)| r >= 1)
+			.map(|(index, _)| index)
+			.collect::<Vec<_>>();
+		
+		// For some reason that I don't understand, this line is not working
+		// A version of this works on the playground, so no idea for now. 
+		let combs = indices.into_iter().combinations(2);
+		
+		out
+}
 
 
 #[derive(Default)]
 pub struct Hypergraph {
-	incidence_matrix: Option<Vec<u8>>, // TODO(jim): decide whether this containiner is right
+	incidence_matrix: Option<Array2<u8>>, 
 	edge_weights: Option<Vec<f32>>,
 	node_weights: Option<Vec<f32>>,
-	edge_list: Option<Vec<u8>>, // TODO(jim): decide on this (internal) type
-	node_list: Option<Vec<u8>>, // TODO(jim): decide on this (internal) type 
+	edge_list: Option<Vec<u8>>, // TODO(jim): decide on this type (HashSet<Vec<u8 / usize>>?)
+	node_list: Option<Vec<u8>>, // TODO(jim): decide on this type (HashSet<u8 / usize>?)
 }
-
-// options for the incidence matrix:
-// Vec<u8> and cause myself all sorts of pain with indexing
-// Multiarray crate: Array2D::new([3, 2], 0); 
-// Vec<Vec<u8>> probably too slow
 
 
 // Idiomatic rust is apparently to have tests and code in the same file
@@ -26,6 +45,7 @@ mod tests {
 
     #[test]
     fn instantiated() {
+		//     ** Copied from the python implementation ** 
 		//     Tests the instantiation of the hypergraph object.
 		//     Pretty simple test as all internal state is set to None.
 		
@@ -37,4 +57,58 @@ mod tests {
 		assert!(h.edge_list.is_none());
 		assert!(h.node_list.is_none());
     }
+	
+	#[test]
+	fn reduced_powerset_t() {
+		// Not part of the python implementation
+		// Tests the function to construct the reduced powerset
+		
+		let data: Array2<u8> = array![
+			[1, 0, 1, 0],
+			[0, 1, 0, 0],
+			[0, 1, 0, 1],
+			[0, 1, 1, 1],
+			[1, 1, 1, 1]
+		];
+		
+		let mut expected = HashSet::new();
+		expected.insert(vec![1,2,3]);
+		expected.insert(vec![1,2]);
+		expected.insert(vec![2,3]);
+		expected.insert(vec![1,3]);
+		
+		assert_eq!(
+			reduced_powerset(data.row(3)),
+			expected
+		);
+		
+	}
+	
+	/*
+	#[test]
+	fn construct_edge_list_t() {
+		// Not part of the python implementation
+		// Tests the function to construct the edge list
+		
+		/*let data = array![
+			[1, 0, 1, 0],
+			[0, 1, 0, 0],
+			[0, 1, 0, 1],
+			[0, 1, 1, 1],
+			[1, 1, 1, 1]
+		];
+		
+		println!("{:?}", data.nrows());
+		println!("{:?}", data.ncols());*/
+		
+	}
+	
+	#[test]
+	fn compute_weights_t() {
+		// Not part of the python implementation
+		// Tests the computation of the weights
+		
+		
+	}
+	*/
 }
