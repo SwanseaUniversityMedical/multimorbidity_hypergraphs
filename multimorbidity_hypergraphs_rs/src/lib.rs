7b5eefcc7f07fad6e0b5d6e6755d59b598c248de
jim-rafferty <james.m.rafferty@gmail.com>
2023-09-05 20:34:07 +0100

    Quite a big change - added some code that should calculate the eigenvector centrality from a previous project. It doesn't completely work and the tests are not complete.

--- multimorbidity_hypergraphs_rs/src/lib.rs
+++ multimorbidity_hypergraphs_rs/src/lib.rs
@@ -13,6 +13,7 @@ use ndarray::{
     ArrayView2, 
     Axis
 };
+use rand::Rng;
 use itertools::Itertools;
 use rayon::prelude::*;
 
@@ -143,6 +144,80 @@ fn construct_edge_list(data: &Array2<u8>) -> Vec<Vec<usize>> {
     
 }
 
+fn standard_deviation(data: Vec<f32>, m: f32) -> f32 {
+
+    let variance = data.iter().map(|value| {
+        let diff = m - (value);
+
+        diff * diff
+    }).sum::<f32>() / data.len() as f32;
+
+
+    variance.sqrt()
+
+}
+
+
+fn iterate_vector(
+    incidence_matrix: &ArrayView2<u8>,
+    weight: &Vec<f32>,
+    eigenvector: &[f32],
+) -> Vec<f32> {
+
+    //let result: Vec<f32>;
+    if let [outer, inner] = &incidence_matrix.shape() {
+        
+        // Happy with this
+        let weighted_incidence: Array2<f32> = Array::from_shape_vec((*outer, *inner), 
+            (0..outer*inner)
+                .into_par_iter()
+                .map(|i| (incidence_matrix[[i / inner, i % inner]] as f32) * weight[i % inner])
+                .collect()
+        ).unwrap();
+
+        
+        // Happy with this
+        let intermediate: Vec<f32> = (0..*inner)
+            .into_par_iter()
+            .map(|k| {
+                (0..eigenvector.len())
+                    .map(|j| weighted_incidence[[j, k]] * eigenvector[j])
+                    .sum::<f32>()
+            })
+            .collect();
+        
+        //happy with this 
+        let term_1: Vec<f32> = (0..eigenvector.len())
+            .into_par_iter()
+            .map(|i| {
+                (0..*inner)
+                    .map(|k| weighted_incidence[[i, k]] * intermediate[k])
+                    .sum()
+            })
+            .collect();
+            
+        // happy with this 
+        let subt: Vec<f32> = (0..eigenvector.len())
+            .map(|i| {
+                (0..*inner)
+                    .into_par_iter()
+                    .map(|k| (incidence_matrix[[i, k]] as f32) * weighted_incidence[[i, k]] * eigenvector[i])
+                    .sum()
+            })
+            .collect();
+            
+        (0..eigenvector.len())
+            .into_par_iter()
+            .map(|i| term_1[i] - subt[i])
+            .collect()
+
+    } else {
+        panic!("The incidence matrix has the wrong shape.");
+    }       
+    //result
+
+}
+
 #[derive(Debug)]
 pub struct Hypergraph {
     incidence_matrix: Array2<u8>, 
@@ -153,9 +228,82 @@ pub struct Hypergraph {
 }
 
 impl Hypergraph {
-    pub fn eigenvector_centrality(&self) -> Vec<f32> {
-        vec![0.0]
+    pub fn eigenvector_centrality(
+        &self, 
+        max_iterations: u32,
+        tolerance: f32,
+    ) -> Vec<f32> {
+        
+        let tmp_inc_mat = &self.incidence_matrix;
+        let im_dims = tmp_inc_mat.shape();
+
+        // generate a random initial estimate for the eigenvector.
+        // Possibly slow but only has to be done once.
+        let mut rng = rand::thread_rng();
+        let mut eigenvector_old: Vec<f32> = vec![0.0; im_dims[0]];
+        for ii in 0..eigenvector_old.len() {
+            eigenvector_old[ii] = rng.gen_range(0.0..1.0);
+        }
+
+        let norm = eigenvector_old.iter().fold(0., |sum, &num| sum + num*num).sqrt();
+        eigenvector_old = eigenvector_old.iter().map(|&b| b / norm).collect();
+
+        let mut eigenvector_new = eigenvector_old.to_owned();
+        let mut eigenvalue: f32 = 0.0;
+
+        for iteration in 0..max_iterations {
+
+            eigenvector_new = iterate_vector(
+                &self.incidence_matrix.view(),
+                &self.edge_weights,
+                &eigenvector_old,
+            );
+
+
+            // calculate the eigenvalue estimate:
+            // 1) find all values of eigenvector_new > 0
+
+            let mut iter_eigenvalue: Vec<f32> = eigenvector_new.iter().zip(
+                eigenvector_old.iter()
+            ).map(|(&a, &b)| {
+                    let mut out = 0.0;
+                    if b > 1e-6 {
+                        out = a / b;
+                    } 
+                    out
+                }
+            ).collect();
+            iter_eigenvalue.retain(|&b| b > 0.0);
+
+            //println!("{:?}", iter_eigenvalue);
+
+            // 2) estimate = mean( #1 above )
+            eigenvalue = iter_eigenvalue.iter().sum::<f32>() / (iter_eigenvalue.len() as f32);
+
+            // 3) error estimate = std( #1 above)    
+            let err_estimate = standard_deviation(iter_eigenvalue, eigenvalue); 
+
+
+            // 4) termination condition: if error_estimate / eigenvector_estimate < tolerance
+                     
+            if err_estimate / eigenvalue < tolerance {
+                println!("Converged after {} iterations", iteration + 1);
+                break;
+            } 
+
+
+            let norm = eigenvector_new.iter().fold(0., |sum, &num| sum + num*num).sqrt();
+            eigenvector_old = eigenvector_new.iter().map(|&b| b / norm).collect();
+
+
+        }
+
+
+        //(eigenvalue, PyArray::from_vec(py, eigenvector_new))
+        eigenvector_new
+    
     }
+    
 }
 
 // Idiomatic rust is apparently to have tests and code in the same file
@@ -522,15 +670,53 @@ mod tests {
         e_vals, e_vecs = np.linalg.eig(a)
         np.abs(e_vecs[:, np.where((e_vals == e_vals.max()))[0][0]])
         
-        
         output: array([0.43576871, 0.52442996, 0.51250412, 0.52193714])
+        
+        
+        NOT weighted resultant
+        
+        import numpy as np 
+        inc_mat = np.array([[1, 1, 1, 1],
+            [0, 0, 1, 1],
+            [1, 0, 1, 1],
+            [1, 1, 1, 0],
+            [0, 1, 0, 1],
+            [1, 0, 0, 1],
+            [0, 1, 1, 0],
+            [1, 1, 0, 0],
+            [0, 1, 1, 1],
+            [1, 1, 0, 1],
+            [1, 0, 1, 0]]
+        )
+        w_e = np.array([0.5, 0.6666667, 0.5, 0.5, 1.0, 0.5, 0.6666667, 0.5, 0.6666667, 0.5, 1.0])
+        w_n = np.array([0.4, 0.8, 0.6, 0.6])
+        
+        a = np.sqrt(inc_mat.T.dot(np.diag(w_e)).dot(inc_mat))
+        np.fill_diagonal(a, 0.0)
+        e_vals, e_vecs = np.linalg.eig(a)
+        np.abs(e_vecs[:, np.where((e_vals == e_vals.max()))[0][0]])
+        
+        output: array([0.48837738, 0.50224377, 0.50694174, 0.50224377])
+        
         */
-        let expected = vec![0.43576871, 0.52442996, 0.51250412, 0.52193714];
+        //let expected = vec![0.43576871, 0.52442996, 0.51250412, 0.52193714];
+        let expected = vec![0.48837738, 0.50224377, 0.50694174, 0.50224377];
+        let e_norm = (expected.iter().fold(0.0, |acc, num| acc + num * num) as f32).sqrt();
         
+        let tol = 0.001;
         let h = compute_hypergraph(&data);
-        let centrality = h.eigenvector_centrality();
+        let centrality = h.eigenvector_centrality(10, tol);
+        let c_norm = (centrality.iter().fold(0.0, |acc, num| acc + num * num) as f32).sqrt();
+        
+        println!("{:?}", expected.iter().map(|x| x / e_norm).collect::<Vec<_>>());
+        println!("{:?}", centrality.iter().map(|x| x / c_norm).collect::<Vec<_>>());
         
         
-        assert_eq!(expected, centrality);
+        assert!(
+            expected.iter().map(|x| x / e_norm)  //.collect::<Vec<_>>(), 
+                .zip(centrality.iter().map(|x| x / c_norm).collect::<Vec<_>>())
+                .map(|(x, y)| (x - y).abs() < tol)
+                .all(|x| x)
+        );
     }
 }
