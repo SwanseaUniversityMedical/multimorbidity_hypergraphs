dbd781b005c036031fa92e9e9e97b3cf271928b4
jim-rafferty <james.m.rafferty@gmail.com>
2023-10-17 23:30:48 +0100

    Moved the original python code to a new directory and set up the initial python rust library.

--- src/lib.rs
+++ src/lib.rs
@@ -7,40 +7,52 @@ use numpy::PyArray2;
 use pyo3::prelude::*;
 
 
-#[pymodule]
-fn multimorbidity_hypergraphs(_py: Python, m: &PyModule) -> PyResult<()> {
-    //m.add_class::<Hypergraph>()?;
-    
-    #[pyclass]
-    pub struct Hypergraph{
-        #[pyo3(get, set)]
-        pub incidence_matrix: Py<PyArray2<u8>>, 
-        #[pyo3(get, set)]
-        pub edge_weights: Vec<f32>,
-        #[pyo3(get, set)]
-        pub node_weights: Vec<f32>,
-        #[pyo3(get, set)]
-        pub edge_list: Vec<Vec<usize>>, 
-        #[pyo3(get, set)]
-        pub node_list: Vec<usize>, 
-    }
-    
-    #[pymethods]
-    impl Hypergraph {
-        #[new]
-        fn new(data: &PyArray2<u8>) -> Self {
-            Python::with_gil(|py| 
+
+#[pyclass]
+pub struct Hypergraph{
+    #[pyo3(get, set)]
+    pub incidence_matrix: Py<PyArray2<u8>>, 
+    #[pyo3(get, set)]
+    pub edge_weights: Vec<f32>,
+    #[pyo3(get, set)]
+    pub node_weights: Vec<f32>,
+    #[pyo3(get, set)]
+    pub edge_list: Vec<Vec<usize>>, 
+    #[pyo3(get, set)]
+    pub node_list: Vec<usize>, 
+}
+
+#[pymethods]
+impl Hypergraph {
+    #[new]
+    fn new(data: Option<&PyArray2<u8>>) -> Self {
+        
+        match data {
+            Some(x) => Python::with_gil(|py| 
                 Hypergraph{
-                    incidence_matrix: PyArray2::zeros(py, [1,1], false).into(), 
-                    edge_weights: vec![0.0],
-                    node_weights: vec![0.0],
-                    edge_list: vec![vec![0]], 
-                    node_list: vec![0], 
+                    incidence_matrix: PyArray2::zeros(py, [0,0], false).into(), 
+                    edge_weights: vec![0.0, 1.0],
+                    node_weights: Vec::new(),
+                    edge_list: vec![Vec::new()], 
+                    node_list: Vec::new(), 
                 }
-            )
+            ),
+            None => Python::with_gil(|py| 
+                Hypergraph{
+                    incidence_matrix: PyArray2::zeros(py, [0,0], false).into(), 
+                    edge_weights: Vec::new(),
+                    node_weights: Vec::new(),
+                    edge_list: vec![Vec::new()], 
+                    node_list: Vec::new(), 
+                }
+            ),
         }
     }
-    
+}
 
+
+#[pymodule]
+pub fn multimorbidity_hypergraphs(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_class::<Hypergraph>()?;
     Ok(())
 }
