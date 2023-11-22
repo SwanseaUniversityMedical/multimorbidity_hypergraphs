50942ac5d8842e93906e10dc926928f3f52c0a93
jim-rafferty <james.m.rafferty@gmail.com>
2023-11-22 17:01:50 +0000

    We're computing quite a lot at the individual level that will be taken forward to compute hyperedge and hyperarc weights later.

--- src/directed_hypergraphs.rs
+++ src/directed_hypergraphs.rs
@@ -2,13 +2,15 @@
 //use crate::types::*;
 
 use ndarray::{
+    array,
     Array,
     Array1,
     Array2,
+    ArrayView1,
     Axis,
     s,
 };
-use std::collections::HashSet;
+use std::collections::{HashSet, HashMap};
 
 /*
 pub fn compute_directed_hypergraph(
@@ -20,16 +22,17 @@ pub fn compute_directed_hypergraph(
 }
 */
 
-fn compute_incidence_matrix(data: &HashSet<Array1<i8>>) -> Array2<i8> {
+
+fn compute_incidence_matrix(progset: &HashSet<Array1<i8>>) -> Array2<i8> {
     
-    let data_vec: Vec<_> = data.into_iter().collect();
+    let progset_vec: Vec<_> = progset.into_iter().collect();
     
-    //let n_arcs = data_vec.len();
-    let n_diseases = data_vec[0].len();
+    let n_diseases = progset_vec[0].len();
+    //let max_hyperedges = 2_usize.pow(n_diseases as u32);
     
     let mut hyperedges: HashSet<Array1<i8>> = HashSet::new();
     
-    for a in data_vec.iter() {
+    for a in progset_vec.iter() {
         
         let mut edge = Array::zeros(n_diseases);
         
@@ -44,8 +47,7 @@ fn compute_incidence_matrix(data: &HashSet<Array1<i8>>) -> Array2<i8> {
         }
         edge[[a[n_conds-1] as usize]] = 1;
 
-        println!("{:?}, {:?}", a.to_vec(), edge.to_vec());
-        hyperedges.insert(edge);
+        hyperedges.insert(edge);        
     }
     
     let n_edges = hyperedges.len();
@@ -59,18 +61,43 @@ fn compute_incidence_matrix(data: &HashSet<Array1<i8>>) -> Array2<i8> {
         
 }
 
+/*
+fn compute_prev_matrices(
+    progset: HashSet<Array1<i8>>
+) -> (Array1<i8>, Array2<i8>) {
+    
+
+    
+}
+*/
+
 fn compute_progset(data: &Array2<i8>) -> HashSet<Array1<i8>> {
     
     let n_rows = data.nrows();
 
     (0..n_rows)
         //.into_iter()
-        .flat_map(|i| compute_single_progset(&data.index_axis(Axis(0), i).to_owned()))
+        .flat_map(|i| compute_single_progset(&data.index_axis(Axis(0), i).to_owned()).0 )
         .collect()
     
 }
 
-fn compute_single_progset(data_ind: &Array1<i8>) -> HashSet<Array1<i8>> {
+fn bincount(arr: &ArrayView1<usize>) -> HashMap<usize, usize> {
+    arr.iter().fold(HashMap::new(), |mut acc, &value| {
+        *acc.entry(value).or_insert(0) += 1;
+        acc
+    })
+}
+
+fn compute_single_progset(
+    data_ind: &Array1<i8>
+) -> (
+    HashSet<Array1<i8>>, // single prog_set
+    Array1<usize>, // bin_tail
+    Array1<i8>, // head_node 
+    Array1<usize>, // bin_headtail
+    Array1<f64> // contribution 
+) {
     
     // NOTE - we are assuming that there are no duplicates in the ordering
     // ie, this is the simplest possible progression. 
@@ -83,16 +110,85 @@ fn compute_single_progset(data_ind: &Array1<i8>) -> HashSet<Array1<i8>> {
     
     
     match n_diseases {
-        1 => HashSet::from([data_ind.clone()]),
+        // people that only have one disease have to be treated spearately
+        1 => {
+            (
+                HashSet::from([data_ind.clone()]),
+                array![0],
+                array![data_ind[0]],
+                array![2_usize.pow(data_ind[0] as u32)],
+                array![1.0],
+            )
+        },
         
-        _ => (1..data_ind.len())
-            .filter(|&i| data_ind[i] >= 0)
-            .map(|i| {
-                let mut i_vec = data_ind.slice(s![0..(i + 1)]).to_vec();
-                i_vec.extend(&vec![-1; data_ind.len() - 1 - i]);
-                Array1::from_vec(i_vec)
-            })
-            .collect(),
+        _ => {
+            let out:HashSet<Array1<i8>> = (1..data_ind.len())
+                .filter(|&i| data_ind[i] >= 0)
+                .map(|i| {
+                    let mut i_vec = data_ind.slice(s![0..(i + 1)]).to_vec();
+                    i_vec.extend(&vec![-1; data_ind.len() - 1 - i]);
+                    Array1::from_vec(i_vec)
+                })
+            .collect();
+            
+            let bin_tail:Array1<_>  = out 
+                .iter()
+                .map(
+                    |arr| arr
+                        .iter()
+                        .filter(|&x| x >= &0)
+                        .fold(0, |acc, x| acc + 2_usize.pow(*x as u32))
+                )
+                .collect();
+            
+            let head_node: Array1<_> = out
+                .iter()
+                .map(
+                    |arr| arr
+                        .iter()
+                        .enumerate()
+                        .filter(|(_, &r)| r >= 0)
+                        .max()
+                        .map(|(index, _)| arr[index])
+                        .unwrap()
+                )
+                .collect();
+                
+            let bin_headtail: Array1<_> = bin_tail 
+                    .iter()
+                    .zip(head_node.clone())
+                    .map(|(x, y)| x + 2_usize.pow(y as u32))
+                    .collect();
+        
+            let n_conds_prog: Array1<_> = out
+                .iter()
+                .map(
+                    |x| x
+                        .iter()
+                        .filter(|&x| x >= &0)
+                        .count()
+                )
+                .collect();
+                
+            let cond_cnt = bincount(&n_conds_prog.view());
+            
+            let contribution: Array1<f64> = n_conds_prog
+                .iter()
+                .map(|x| 1.0 / (cond_cnt[x] as f64))
+                .collect();
+            
+            (    
+                out, // single prog_set
+                
+                bin_tail, 
+                
+                head_node,
+                
+                bin_headtail,
+                    
+                contribution,
+            )
+        },
     }
 }
 
@@ -115,7 +211,9 @@ mod tests {
         
         let out = compute_single_progset(&data);
         
-        assert_eq!(out, expected);
+        assert_eq!(out.0, expected);
+        
+        //assert!(false);
     }
     
     
@@ -127,7 +225,9 @@ mod tests {
         
         let out = compute_single_progset(&data);
         
-        assert_eq!(out, expected);
+        assert_eq!(out.0, expected);
+        
+        //assert!(false);
     }
     
     #[test]
